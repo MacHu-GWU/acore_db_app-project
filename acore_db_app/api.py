25d22836a3de4f810929ecd789951a3312bb5051
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-06-27 12:53:46 -0400

    [no ci] update api

--- acore_db_app/api.py
+++ acore_db_app/api.py
@@ -1,2 +1,4 @@
 # -*- coding: utf-8 -*-
 
+from .orm import Orm
+from .app import api as app
--- debug/debug_db_app.py
+++ debug/debug_db_app.py
@@ -3,9 +3,8 @@
 from boto_session_manager import BotoSesManager
 from acore_db_ssh_tunnel.api import create_engine
 from acore_server.api import Server
-from acore_db_app.orm import Orm
-from acore_db_app.app.locale import LocaleEnum
-from acore_db_app.app.quest import list_quest_by_character, complete_latest_n_quest
+
+from acore_db_app.api import Orm, app
 from rich import print as rprint
 
 bsm = BotoSesManager(profile_name="bmt_app_dev_us_east_1")
@@ -24,11 +23,11 @@ engine = create_engine(
 )
 orm = Orm(engine=engine)
 
-# res = list_quest_by_character(orm, "sa")
-# rprint(res)
+res = app.list_quest_by_character(orm, "sa")
+rprint(res)
 
-# complete_latest_n_quest(
-#     orm=orm,
-#     character="sa",
-#     locale=LocaleEnum.zhCN,
-# )
+app.complete_latest_n_quest(
+    orm=orm,
+    character="sa",
+    locale=app.LocaleEnum.zhCN,
+)
