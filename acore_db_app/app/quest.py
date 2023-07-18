93832b5d36d7a4d567191cdc3627bc09bb40146b
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-07-17 21:01:00 -0400

    Update quest.py

--- acore_db_app/app/quest.py
+++ acore_db_app/app/quest.py
@@ -179,7 +179,7 @@ def get_enriched_quest_data(
             selects.append(
                 orm.t_quest_template_locale.c.Title.label("quest_title_locale")
             )
-            joins = joins.join(
+            joins = joins.left(
                 # 获得任务的文本信息
                 orm.t_quest_template_locale,
                 orm.t_quest_template_locale.c.ID == orm.t_character_queststatus.c.quest,
