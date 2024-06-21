747e10b1c8e12b093608107dd5eadadb5b429aa7
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2024-06-21 13:53:53 -0400

    0.2.3

--- debug/debug_ssh_tunnel.py
+++ debug/debug_ssh_tunnel.py
@@ -6,13 +6,12 @@ from acore_server.api import Server
 
 bsm = BotoSesManager(profile_name="bmt_app_dev_us_east_1")
 env_name = "sbx"
-server_name = "green"
-path_pem_file = Path.home() / "ec2-pem" / "bmt-app-dev" / "us-east-1" / "sanhe-dev.pem"
+server_name = "blue"
 server_id = f"{env_name}-{server_name}"
 
 server = Server.get(bsm, server_id)
 
-server.create_ssh_tunnel(path_pem_file)
-# server.list_ssh_tunnel(path_pem_file)
-server.test_ssh_tunnel()
-# server.kill_ssh_tunnel(path_pem_file)
+# server.create_ssh_tunnel(bsm=bsm)
+# server.list_ssh_tunnel(bsm=bsm)
+# server.test_ssh_tunnel()
+# server.kill_ssh_tunnel(bsm=bsm)
